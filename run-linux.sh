78816d8c8999b1f2df65abd8724bd824861aa45c
Urban Versis 32 <107663697+UrbanVersis32@users.noreply.github.com>
2024-04-07 15:46:29 -0500

    Update run-linux.sh

--- run-linux.sh
+++ run-linux.sh
@@ -3,4 +3,4 @@
 
 echo "This is the Linux PyScreenSaver Runner.\n"
 
-python3 source/screensaver.py || echo "You need to install Python to run this program"
+python3 source/screensaver.py || echo "You need to install or update Python to run this program"
